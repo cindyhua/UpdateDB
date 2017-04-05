85717f44604fee5ebe0f953c1476f56b6bc4e348
cindyhua <serenaxin@qq.com>
2017-04-05 23:00:09 +0800

    simple code in DBHandle

--- UpdateDB/DBHandle.h
+++ UpdateDB/DBHandle.h
@@ -18,39 +18,99 @@ private:
 	string	defaultvalue;
 };
 
-typedef map<Param, string> ParamValue;
-typedef map<string, Param> ParamList;
+typedef map<Param, string> mapParamValue;
+typedef map<string, mapParamValue> mapParamList;
 
-
+class SAConnection;
 template <typename T>
 class CDBHandle
 {
 public:
-	CDBHandle();
-	virtual ~CDBHandle();
+	CDBHandle() :_data(NULL), _conn(NULL){};
+	~CDBHandle();
 
 
 	T* _data;
-	Reader<T>* _rdData;
-	bool bindReaderData(Reader<T>& rdData) {
-		_rdData = &rdData;
+	SAConnection* _conn;
+	string _tablename;
+	uint32_t _commitcount;
+	/*
+	db.bind(td,conn)
+	db.iniSQL("tiger",20);;
+	*/
+	bool bind(T& data,SAConnection& conn) {
+		_data = &data;
+		return true;
+	}
+	
+	
+	bool iniSQL(const char* tablename,uint32_t muticount=1) {
+		bindParam(muticount);
+
+
+		return true;
+	}
+	bool setSQL(const char* sql)
+	{
+		_updateSQL = sql;
+		return true;
+	}
+	bool setCommitCount(uint32_t commitcount)
+	{
+		_commitcount = commitcount;
+		return true;
+	}
+	bool release()
+	{
+		_data = NULL;
+		_conn = NULL;
+		_tablename = "";
 		return true;
 	}
 
-	bool iniParam()
-	{
-		return true;
-	}
+	string _updateSQL;
+	string _insertSQL;
 
-	bool putParamValue(T& data)
+	/*
+	批量更新语句(从文本）:
+	1.bulk(sqlserver)
+	2.load(mysql)
+	*/
+	string _bulkSQL;
+	//通过一次多条参数绑定，实现批量，形如:insert table(a,b,c) values(?,?,?),(?,?,?)....
+	bool bindParam(uint32_t coumuticountnt=1)
 	{
-		for (DataField* it = &(data.begin()); !data.end(); it = &(data.next()))
+		//clear at first
+
+
+		//then bind
+		for (uint32_t i = 0; i++; i < muticount)
 		{
-			//it->pvalue;
+			for (DataField* it = &(_data->begin()); !_data->end(); it = &(_data->next()))
+			{
+				//it->pvalue;
+			}
 		}
+		return true;
+	}
+
+	bool putParamValue()
+	{
+		return putParamValue(*_data);
 	};
 
 
+	bool putParamValue(T& data)
+	{
+		for (DataField* it = &(data->begin()); !data->end(); it = &(data->next()))
+		{
+			//it->pvalue;
+		}
+		return true;
+	};
+
+
+	
 
 };
 
